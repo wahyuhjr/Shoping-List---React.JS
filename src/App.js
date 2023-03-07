c71466f49fe0750a3a1ca9c86f7b3ca80906a269
wahyuhjr <wahyuhidjr11@gmail.com>
2023-03-07 10:48:47 +0700

    set navbar and form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from "react"
+import './App.css'
+import shoppingIcon from './assets/shopping-icon.svg'
+
 function App() {
+
+  const [count, setCount] = useState(0); //tupple count
+
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+   <>
+    <nav className="nav">
+      <img className="nav-icon" src={shoppingIcon} alt="shopping icon"/>
+      <h1 className="nav-title">Shopping List</h1>
+    </nav>
+
+    <section className="container">
+      <form className="form">
+        <input
+          className="input" type="text" placeholder="List" 
+        />
+        <button className="add-button" type="submit">Add</button>
+      </form>
+    </section>
+   </>
   );
 }
+
+
 
 export default App;
