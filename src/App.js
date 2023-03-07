49e04aab7a6e41e2936e920f4f970ab37d2ddf7f
wahyuhjr <wahyuhidjr11@gmail.com>
2023-03-07 11:38:00 +0700

    create todo element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { useState } from "react"
 import './App.css'
 import shoppingIcon from './assets/shopping-icon.svg'
+import plusIcon from './assets/plus-icon.svg'
+import minusIcon from './assets/minus-icon.svg'
+import classnames from 'classnames'
 
 function App() {
 
   const [value, setValue] = useState('')
-  console.log(value)
+  const [todos, setTodos] = useState([
+    {title:"Tisu", count:1},
+    {title:"Sabun", count:1},
+    {title:"Sikat", count:1},
+    {title:"Minum", count:1},
+  ])
 
   return (
    <>
@@ -22,6 +30,35 @@ function App() {
         />
         <button className="add-button" type="submit">Add</button>
       </form>
+
+      {/* callback function */}
+      {todos.length > 0 ? (
+        <div className="todos">
+        {todos.map((todo, index, arr) => {
+          return (
+            <div key={index} className={`todo ${!(arr.length === index + 1) && ('todo-devider')}`}>
+
+            {todo.title}
+
+              <div className="todo-icon">
+                <div className="todo-count">{todo.count}</div>
+
+                <button className="todo-action-button">
+                  <img src={minusIcon} alt="minus icon"/>
+                </button>
+
+                <button className="todo-action-button">
+                  <img src={plusIcon} alt="plus icon"/>
+                </button>
+
+              </div>
+            </div>
+          )}
+          )}
+        </div>
+      ) : (
+        <div></div>
+      )}
     </section>
    </>
   );
